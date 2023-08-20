a84184c6a9d95bcf19e9bdeeb0ce9104cd666156
luangitdev <luaninveste@gmail.com>
2023-08-19 23:59:24 -0300

    Implementada a função montarBlog()

--- js/script.js
+++ js/script.js
@@ -12,3 +12,15 @@ function loadPosts(){
             document.getElementById("posts").innerHTML = 'Erro na coleta do JSON'
         });
 }
+
+function montarBlog(lista){
+    let html = '';
+
+    for(let i in lista){
+        html += '<h3>'+lista[i].title+'</h3>'
+        html += lista[i].body+'<br/>'
+        html += '<hr/>'
+    }
+
+    document.getElementById("posts").textContent = html;
+}
